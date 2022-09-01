c175298c3ec265e41889ac0fb13c9c389cf270f5
Samuel Park <s.park111497@gmail.com>
2022-08-31 23:18:57 -0700

    finished bubble sort exercise

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,25 @@
 
 function bubbleSort(arr) {
-
-    // Iterate through the array
+  let swapped = false;
+  // Iterate through the array
+  for (let i = 0; i < arr.length; i++) {
 
       // If the current value is greater than its neighbor to the right
-        // Swap those values
+      if (arr[i] > arr[i + 1]) {
 
-        // Do not move this console.log
+        // Swap those values
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+
         console.log(arr.join(","));
 
-    // If you get to the end of the array and no swaps have occurred, return
-
-    // Otherwise, repeat from the beginning
-
+        swapped = true;
+      }
+  }
+    // If you get to the end of the array and swaps have occured, do it again
+    if (swapped) {
+      bubbleSort(arr);
+    }
+    return arr;
 }
 
 module.exports = bubbleSort;
